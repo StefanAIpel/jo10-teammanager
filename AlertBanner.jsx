4439d298274ee35337f7e8418b37cd1013b8fd61
StefanAIpel <130749432+StefanAIpel@users.noreply.github.com>
2026-01-15 22:26:31 +0100

    Delete AlertBanner.jsx

--- AlertBanner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function AlertBanner({ message }) {
-  return (
-    <div className="alert-banner" dangerouslySetInnerHTML={{ __html: '🔔 ' + message }} />
-  );
-}
-
-export default AlertBanner;
